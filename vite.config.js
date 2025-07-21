523abe694be0db2e73fd9ac073a524d96bcf66c8
Yuri Alves <yuribarbearia@icloud.com>
2025-07-21 11:45:44 -0300

    feat: Adiciona script de deploy e corrige config do Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: "/calculadora-imc-vue/", // <--- ERRO AQUI! A chave fecha o objeto cedo demais.
   resolve: {
+    // <--- 'resolve' está fora do objeto principal.
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+}); // <--- E ESSE '+)' EXTRA AQUI.
